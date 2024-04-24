0ec70fa528806e82b5fe904ece3f077bc1e87b59
Colin Leung <colin@cowise.co>
2024-04-25 04:39:24 +0800

    fix: index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import url from "node:url";
 const dirname = url.fileURLToPath(new URL('.', import.meta.url)).replace(/\/$/, '');
 export default {dirname}
 
-import ControllerContent from './classes/controller/Content';
-import ControllerMixinContent from './classes/controller-mixin/Content';
-import HelperLabel from './classes/helper/Label';
-import HelperPageText from './classes/helper/PageText';
+import ControllerContent from './classes/controller/Content.mjs';
+import ControllerMixinContent from './classes/controller-mixin/Content.mjs';
+import HelperLabel from './classes/helper/Label.mjs';
+import HelperPageText from './classes/helper/PageText.mjs';
 
 export {
   ControllerContent,
